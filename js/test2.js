75a646449a29ed13dcadf2ce18718ae64ab7e7d6
ramon <ramon@ramon-pc.(none)>
2014-08-15 17:33:37 +0200

    Test 2 update

--- js/test2.js
+++ js/test2.js
@@ -6,6 +6,9 @@ var datasource = "data/data.csv"
 var width = 960,
     height = 500;
 
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
 var y = d3.scale.linear()
     .range([height, 0]);
 
@@ -14,22 +17,21 @@ var chart = d3.select(".chart")
     .attr("height", height);
 
 d3.csv(datasource, type, function(error, data) {
+  x.domain(data.map(function(d) { return d.name; }));
   y.domain([0, d3.max(data, function(d) { return d.value; })]);
-
-  var barWidth = width / data.length;
 
   var bar = chart.selectAll("g")
       .data(data)
     .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+      .attr("transform", function(d) { return "translate(" + x(d.name) + ",0)"; });
 
   bar.append("rect")
       .attr("y", function(d) { return y(d.value); })
       .attr("height", function(d) { return height - y(d.value); })
-      .attr("width", barWidth - 1);
+      .attr("width", x.rangeBand());
 
   bar.append("text")
-      .attr("x", barWidth / 2)
+      .attr("x", x.rangeBand() / 2)
       .attr("y", function(d) { return y(d.value) + 3; })
       .attr("dy", ".75em")
       .text(function(d) { return d.value; });
@@ -39,4 +41,5 @@ function type(d) {
   d.value = +d.value; // coerce to number
   return d;
 }
+
     
